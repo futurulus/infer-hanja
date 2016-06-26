a0a68bbcefe71eeeb2a89ddae212431b35ba7110
Will Monroe <wmonroe4@stanford.edu>
2016-06-26 11:16:00 -0700

    Add dictionary features, parallelize featurization

--- classifier.py
+++ classifier.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import multiprocessing
 import numpy as np
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LogisticRegression
@@ -8,36 +9,53 @@ from stanza.monitoring import progress
 from stanza.research import config
 from stanza.research.learner import Learner
 
+import kengdict
+
 
 class ClassifierLearner(Learner):
     def __init__(self):
         self.classifiers = {}
+        self.label_dicts = {}
         self.singletons = {}
 
     def train(self, training_instances, validation_instances='ignored', metrics='ignored'):
         training_instances = list(training_instances)
 
-        features, labels, _ = self.data_to_arrays(training_instances)
-
-        progress.start_task('Character', len(labels))
-        for i, hangul in enumerate(labels):
-            progress.progress(i)
-
+        print('Assembling label sets')
+        # Vectorize once to get the labels for the dictionary features
+        _, labels, _ = self.data_to_arrays(training_instances)
+        for hangul in labels:
             enc = LabelEncoder()
-            arr_labels = enc.fit_transform(labels[hangul])
+            enc.fit(labels[hangul])
             if len(enc.classes_) == 1:
                 self.singletons[hangul] = enc.classes_[0]
                 continue
             else:
                 assert len(enc.classes_) > 1
+                self.label_dicts[hangul] = enc
+
+        print('Featurizing')
+        # Vectorize a second time to fill in the dictionary feature values
+        features, labels, _ = self.data_to_arrays(training_instances)
+
+        print('Training')
+        progress.start_task('Character', len(labels))
+        for i, hangul in enumerate(labels):
+            progress.progress(i)
+
+            if hangul in self.singletons:
+                continue
 
             vec = DictVectorizer()
             arr_features = vec.fit_transform(features[hangul])
 
+            enc = self.label_dicts[hangul]
+            arr_labels = enc.transform(labels[hangul])
+
             model = LogisticRegression(solver='lbfgs', multi_class='multinomial')
             model.fit(arr_features, arr_labels)
 
-            self.classifiers[hangul] = (model, vec, enc)
+            self.classifiers[hangul] = (model, vec, self.label_dicts[hangul])
         progress.end_task()
 
     @property
@@ -48,9 +66,11 @@ class ClassifierLearner(Learner):
         return total
 
     def predict_and_score(self, eval_instances):
+        print('Featurizing')
         features, _, indices = self.data_to_arrays(eval_instances)
         pred_labels = {}
 
+        print('Predicting')
         progress.start_task('Character', len(features))
         for i, hangul in enumerate(features):
             progress.progress(i)
@@ -77,43 +97,83 @@ class ClassifierLearner(Learner):
         return predictions, scores
 
     def data_to_arrays(self, insts):
+        options = config.options()
+
+        worker_pool = multiprocessing.Pool(8)
+
         features = defaultdict(list)
         labels = defaultdict(list)
         indices = []
+        progress.start_task('Sentence', len(insts))
         for i, inst in enumerate(insts):
+            progress.progress(i)
+
             assert len(inst.input) == len(inst.output), inst.__dict__
             indices_inst = []
+            featurizer_args = []
             for j, (hangul, hanja) in enumerate(zip(inst.input, inst.output)):
+                if hangul in self.label_dicts:
+                    classes = self.label_dicts[hangul].classes_
+                else:
+                    classes = []
+
                 indices_inst.append(len(labels[hangul]))
-                features[hangul].append(self.featurize(inst.input, j))
+                featurizer_args.append((inst.input, j, classes, options.features))
                 labels[hangul].append(hanja)
+
+            for hangul, feats in worker_pool.map(featurize, featurizer_args):
+                features[hangul].append(feats)
+
             indices.append(indices_inst)
+        progress.end_task()
         return features, labels, indices
 
-    def featurize(self, input, idx):
-        options = config.options()
-        feats = {}
-        for feat_type in options.features:
-            feats.update(FEATURES[feat_type](input, idx))
-        return feats
+
+def featurize(args):
+    input, idx, hanjas, feat_types = args
+    feats = {}
+    for feat_type in feat_types:
+        feats.update(FEATURES[feat_type](input, idx, hanjas))
+    return input[idx], feats
 
 
-def next_feature(input, idx):
+def next_features(input, idx, hanjas):
     return {'next': input[idx + 1:idx + 2]}
 
 
-def prev_feature(input, idx):
+def prev_features(input, idx, hanjas):
     return {'prev': input[max(0, idx - 1):idx]}
 
 
-def sent_boc_feature(input, idx):
+def sent_boc_features(input, idx, hanjas):
     return {u'sent:' + c: 1 for i, c in enumerate(input) if i != idx}
 
 
+def dict_features(input, idx, hanjas):
+    feats = {}
+    for hanja in hanjas:
+        for entry in kengdict.entries_for_hanja(hanja):
+            for pos in find_all(entry.hanja, hanja):
+                if input[idx - pos:idx - pos + len(entry.hangul)] == entry.hangul:
+                    feats[u'dictentry%s' % len(entry.hangul)] = hanja
+    return feats
+
+
+def find_all(haystack, needle):
+    start = 0
+    while True:
+        pos = haystack.find(needle, start)
+        if pos == -1:
+            break
+        yield pos
+        start = pos + 1
+
+
 FEATURES = {
-    'next': next_feature,
-    'prev': prev_feature,
-    'sent_boc': sent_boc_feature,
+    'next': next_features,
+    'prev': prev_features,
+    'sent_boc': sent_boc_features,
+    'dict': dict_features,
 }
 
 parser = config.get_options_parser()
---
+++ kengdict.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import sys
+import unicodecsv as csv
+from collections import namedtuple, defaultdict
+
+Entry = namedtuple('Entry', ['hanja', 'hangul', 'english'])
+
+
+class KEngDict(object):
+    def __init__(self, infile=None):
+        self.entries = []
+        self.hanja_index = defaultdict(list)
+        if infile:
+            rdr = csv.reader(infile, dialect=csv.excel_tab)
+            rows = list(rdr)
+            for row in rows:
+                hanjas = row[9].split(',')
+                for j in hanjas:
+                    self.add_entry(row[1], j, row[3])
+
+    def add_entry(self, hangul, hanja, english):
+        hangul, hanja = strip_dittos(hangul, hanja)
+        if hanja is None:
+            return
+        entry = Entry(hangul, hanja, english)
+        self.entries.append(entry)
+        for j in set(hanja):
+            self.hanja_index[j].append(entry)
+
+    def entries_for_hanja(self, hanja):
+        return self.hanja_index[hanja]
+
+
+_dict = None
+
+
+def load_dict():
+    global _dict
+    if _dict is None:
+        with open('data/restricted/kengdic_2011.tsv', 'r') as infile:
+            _dict = KEngDict(infile)
+    return _dict
+
+
+def entries_for_hanja(hanja):
+    d = load_dict()
+    return d.entries_for_hanja(hanja)
+
+
+def strip_dittos(hangul, hanja):
+    u'''
+    Fills in the full hanja version of a dictionary entry that might
+    contain '-' and '~' placeholders.
+
+    "NULL" hanja are replaced with the hangul.
+
+    >>> g, j = strip_dittos(u'갈비', u'NULL'); print g; print j
+    갈비
+    갈비
+
+    Placeholders at the end are stripped; these consist overwhelmingly of
+    variable endings like '하다'.
+
+    >>> g, j = strip_dittos(u'제거하다', u'除去~~'); print g; print j
+    제거
+    除去
+    >>> g, j = strip_dittos(u'제거하다', u'除去'); print g; print j
+    제거
+    除去
+    >>> g, j = strip_dittos(u'가공품', u'加工品-'); print g; print j
+    가공품
+    加工品
+    >>> g, j = strip_dittos(u'가능해지다', u'可能-'); print g; print j
+    가능
+    可能
+    >>> g, j = strip_dittos(u'감미로운', u'甘味---'); print g; print j
+    감미
+    甘味
+
+    Placeholders at the beginning are replaced with the hangul; these
+    are often content words.
+
+    >>> g, j = strip_dittos(u'갈비탕', u'-湯'); print g; print j
+    갈비탕
+    갈비湯
+    '''
+    # orig_hangul, orig_hanja = hangul, hanja
+
+    if hanja == 'NULL':
+        return hangul, hangul
+
+    while hanja.endswith('-') or hanja.endswith('~'):
+        hanja = hanja[:-1]
+
+    ditto_start = False
+    while hanja.startswith('-') or hanja.startswith('~'):
+        ditto_start = True
+        hanja = hanja[1:]
+
+    if not ditto_start and len(hanja) <= len(hangul):
+        return hangul[:len(hanja)], hanja
+    elif ditto_start and len(hanja) <= len(hangul):
+        return hangul, hangul[:-len(hanja)] + hanja
+    else:
+        # sys.stderr.write('kengdict: could not fill in missing hanja: %s [%s]\n' %
+        #                  (orig_hangul.encode('utf-8'), orig_hanja.encode('utf-8')))
+        return None, None
